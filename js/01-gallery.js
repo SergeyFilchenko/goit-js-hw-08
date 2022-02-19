a892d3463c50c19f4166c0de15eb26e09fce6713
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-02-19 12:20:34 +0200

    add packaje.json

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
