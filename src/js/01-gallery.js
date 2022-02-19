92893163aee809ad5c70fdd0976dd11f6d883e00
SergeyFilchenko <87279118+SergeyFilchenko@users.noreply.github.com>
2022-02-19 16:25:42 +0200

    upd galary.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Описан в документации
 import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
